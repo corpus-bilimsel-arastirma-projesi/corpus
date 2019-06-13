fa1ab7137a8954ae11b65a255f1b49a79e2535e4
dedinededin <ibrahimmdogann@gmail.com>
2019-06-13 17:39:27 +0300

    [EXPERIMENT] MEDIA URL

--- backend/urls.py
+++ backend/urls.py
@@ -32,5 +32,5 @@ urlpatterns = [
     re_path(r'^.*$', index_view, name='index'),
 ]
 
-if True:  # [EXPERIMENTAL]
+if settings.DEBUG:
     urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + urlpatterns
