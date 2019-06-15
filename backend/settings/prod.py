3a1a7e8b7abc0bf9e07266f1e353d80016311408
tugberkgoc <tugberkgoc@gmail.com>
2019-06-15 15:33:58 +0300

    [FIX] Heroku Fix

--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -18,7 +18,8 @@ DATABASES = {
 # SECURITY #
 ############
 
-DEBUG = bool(os.getenv('DJANGO_DEBUG', ''))
+# DEBUG = bool(os.getenv('DJANGO_DEBUG', ''))
+DEBUG = True
 
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', SECRET_KEY)
 
